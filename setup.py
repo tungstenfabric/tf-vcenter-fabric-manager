a17abdf4b0fc1d0e9c04b4c2844f8112c402ddbe
Andrey Pavlov <andrey-mp@yandex.ru>
2020-05-20 22:10:57 +0300

    fix using filter(...) for python3 and fix gevent/geventhttpclient issues
    python2 returns list for this object but python3 returns 'class filter'
    so we must convert it to list where list is required
    
    related-jira-bug: CEM-14275
    Change-Id: Icc5eac51a37c920896b3335008f2d148b2abbc20

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def requirements(filename):
     with open(filename) as f:
         lines = f.read().splitlines()
     c = re.compile(r"\s*#.*")
-    return filter(bool, map(lambda y: c.sub("", y).strip(), lines))
+    return lsit(filter(bool, map(lambda y: c.sub("", y).strip(), lines)))
 
 
 setup(
